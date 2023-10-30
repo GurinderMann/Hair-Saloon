8d5bf9d4336802a11c256dd6b27e8b6f64c6d3a4
GurinderMann <manngurinder110@gmail.com>
2023-10-30 15:17:59 +0100

    Styling home page

--- Parrucchiere/Controllers/HomeController.cs
+++ Parrucchiere/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Parrucchiere.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,11 +9,23 @@ namespace Parrucchiere.Controllers
 {
     public class HomeController : Controller
     {
+        ModelDbContext db = new ModelDbContext();
+       
         public ActionResult Index()
         {
+            var servizi = db.Servizi.ToList()
+              .Select(s => new SelectListItem
+              {
+                  Value = s.IdServizio.ToString(),
+                  Text = s.Tipo + " - " + s.Costo.ToString("C")
+              }).ToList();
+
+           
+
+            ViewBag.TipoOptions = servizi;
             return View();
         }
-
+      
        
     }
 }
---
+++ Parrucchiere/Controllers/ProfiloController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Parrucchiere.Controllers
+{
+    public class ProfiloController : Controller
+    {
+        // GET: Profilo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
